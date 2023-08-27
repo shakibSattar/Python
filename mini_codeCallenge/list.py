d07877d19dd0ab4316e4ec1c47ee90eae5249d67
Shakiba <shakiba.laiba@gmail.com>
2023-08-27 13:59:38 -0700

    another list challange added

--- mini_codeCallenge/list.py
+++ mini_codeCallenge/list.py
@@ -2,8 +2,17 @@
 #declere a  function
 def corrList(mylist):
     mylist[-1] = 'and ' + mylist[-1]
-    mylist = ','.join(mylist)
-    return mylist
+    return ','.join(mylist)
+    
 #call function to print list
 print(corrList(['car', 'bus', 'bike', 'truck']))
 print(corrList(['apple', 'banana', 'mango', 'cherry']))
+
+##---declere a function that turns list and number in to human sentance 
+def listNum(myList):
+    if myList[0] == 1:
+        return 'there is ' + str(myList[0]) + ' ' + myList[1] + '.'
+    else:
+        return 'there are '+ str(myList[0]) + ' ' + myList[1] + 's.'
+print(listNum(['5', 'cat']))
+print(listNum(['1', 'dog']))
