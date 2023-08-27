770c1120ec942e7c375477a65f19becb04da612a
Shakiba <shakiba.laiba@gmail.com>
2023-08-26 22:02:31 -0700

    function for opposit case of string added

---
+++ lowerCase.py
@@ -0,0 +1,37 @@
+#---fuction that returns all capital letter to lower case string 
+#declere function
+def LowerCase(string):
+    return string.lower()
+#call funtion to print line
+print(LowerCase('LOVE TO BREAK HATE'))
+print(LowerCase('SHAMELESS LOVE'))
+
+
+
+#---declere a function to return string  in upper caseletter
+def UpperCase(string):
+    return string.upper()
+#call function to print string 
+print(UpperCase('love to advanture'))
+print(UpperCase('proud on your own efforts')) 
+
+
+
+##---declere a function that returns a string to oppoisit letter character case
+def switchCase(string):
+    newString = ''
+    for letters in string:
+        if letters.islower():
+            newString += letters.upper()
+        else:
+            newString += letters.lower()
+    return newString
+#call function to print string
+print(switchCase('apple'))
+print(switchCase('HELLO hello Hello'))
+print(switchCase('Welcome To My Channel'))
+
+
+
+
+         
