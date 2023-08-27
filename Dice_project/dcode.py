069d4d573121079be1d1b55bbd9a00bae246b022
Shakiba <shakiba.laiba@gmail.com>
2023-08-26 19:00:51 -0700

    adde filles two giles for python code challanges

--- Dice_project/dcode.py
+++ Dice_project/dcode.py
@@ -4,5 +4,5 @@ while True:
     user  = int(input("Enter number of Dice: "))
     if user == 0:
         break
-    Dice.findDice(user)
+    Dice.findDice(user) #function is called
     
---
+++ mini_codeCallenge/lowerCase.py
@@ -0,0 +1,16 @@
+##---fuction that returns all capital letter to lower case string 
+#declere function
+def LowerCase(string):
+    return string.lower()
+#call funtion to print line
+print(LowerCase('LOVE TO BREAK HATE'))
+print(LowerCase('SHAMELESS LOVE'))
+
+
+#declere a function to return string  in upper caseletter
+def UpperCase(string):
+    return string.upper()
+#call function to print string 
+print(UpperCase('love to advanture'))
+print(UpperCase('proud on your own efforts'))
+
