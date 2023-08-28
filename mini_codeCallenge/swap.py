20dd3aaaad862e4e4cbece4427ab2e5ec0d54293
Shakiba <shakiba.laiba@gmail.com>
2023-08-28 16:33:34 -0700

    file for swaped twon numbers  is added

---
+++ mini_codeCallenge/swap.py
@@ -0,0 +1,10 @@
+##--User will input (2numbers).Write a program to swap the numbers--
+
+num1 = input('Enter number 1: ')
+num2 = input('Enter number 2: ')
+
+swap = num1
+num1 = num2
+num2 = swap
+
+print(f'the swaped number for number1 is {num1} and for number2 is {num2}')
