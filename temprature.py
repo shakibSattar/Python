15db0d447db04036261b327092b11390e5f389d2
Shakiba <shakiba.laiba@gmail.com>
2023-08-28 16:16:05 -0700

    a file to convert celsius temprature to fahrenheit is added

---
+++ temprature.py
@@ -0,0 +1,5 @@
+##---convert celsius value to fahrenheit---
+temp = float(input('Enter tembprature in in  celsius: '))
+
+fahrenheit = (temp * 1.8) + 32
+print(f'the {temp} temprature in celsius is equal to {fahrenheit} fahrenheit')
